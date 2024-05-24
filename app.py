d4d880507e487ba10b4a55ced3cb62528e3e8ff8
James Baker <--global>
2024-05-24 16:55:34 +0100

    Added traces & videos of tests to test test

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template
 
+from download_lincat_file import *
+
 app = Flask(__name__, static_folder="static/")
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
+
+@app.route("/automation-builder")
+def automation_builder():
+    return render_template("automation.html")
+
+@app.route("/test-automation/<string:supplier>")
+def test_automation(supplier: str):
+    ## test the automation for the supplier passed
+    download_file()
+    return "Tested!"
--- download_lincat_file.py
+++ download_lincat_file.py
@@ -2,30 +2,36 @@ from playwright.sync_api import Page, sync_playwright
 import time
 
 
-def download_file(page: Page):
-    page.goto("https://www.dropbox.com/sh/jys76901q44nxpr/AACYu9RfSCE4-BDbGIkBKaJUa?e=1&dl=0")
-
-    ccpa = page.frame_locator("#ccpa-iframe")
-    accept = ccpa.locator("#accept_all_cookies_button")
-    accept.click()
-
-    time.sleep(2)
-    page.get_by_role("button", name="Download").click()
-    time.sleep(2)
-
-    with page.expect_download() as download_info:
-        # Perform the action that initiates download
-        page.get_by_text("Or continue with download only", exact=True).click()
-    download = download_info.value
-
-    download.save_as("downloads/" + download.suggested_filename)
-
-if __name__ == "__main__":
+def download_file():
     with sync_playwright() as pw:
         chromium = pw.chromium
         browser = chromium.launch(headless=False)
-        page = browser.new_page()
+        context = browser.new_context(record_video_dir="recordings/videos/")
+        context.tracing.start(screenshots=True, snapshots=True, sources=True)
+        page = context.new_page()
+        time.sleep(0.5)
+        page.goto("https://www.dropbox.com/sh/jys76901q44nxpr/AACYu9RfSCE4-BDbGIkBKaJUa?e=1&dl=0")
+
+        ccpa = page.frame_locator("#ccpa-iframe")
+        accept = ccpa.locator("#accept_all_cookies_button")
+        accept.click()
+
         time.sleep(2)
-        download_file(page)
+        page.get_by_role("button", name="Download").click()
         time.sleep(2)
+
+        with page.expect_download() as download_info:
+            # Perform the action that initiates download
+            page.get_by_text("Or continue with download only", exact=True).click()
+        download = download_info.value
+
+        download.save_as("downloads/" + download.suggested_filename)
+        
+        print(page.video.path)
+        time.sleep(2)
+        context.tracing.stop(path="recordings/traces/trace.zip")
+        context.close()
         browser.close()
+
+if __name__ == "__main__":
+    download_file()
