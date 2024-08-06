50fbd2884ebccf2da9dd3d24e7675e91b0fc945f
James Baker <--global>
2024-08-06 13:34:04 +0100

    Fixed bug where all were listed as Downloads

--- automations.py
+++ automations.py
@@ -4,7 +4,7 @@ from database import query_db, insert_to_db, delete_from_db
 def create_automation(type: int, url: str, location: str, name: str, supplier_id: str) -> int|None:
     return insert_to_db(
         "INSERT INTO automations (type, url, location, name, supplier_id) VALUES (:type,:url,:location,:name,:supplier_id)",
-        {"type":1, "url":url, "location":location, "name":name, "supplier_id":int(supplier_id)}
+        {"type":type, "url":url, "location":location, "name":name, "supplier_id":int(supplier_id)}
     )
 
 def delete_automation(automation_id: str, supplier_id: str) -> List[str]|str|None:
