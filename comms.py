baab4449577fe830bf1e454f2381b186ce23cb3b
James Baker <--global>
2024-08-16 15:02:04 +0100

    Added functionality to post to Cliq and test

---
+++ comms.py
@@ -0,0 +1,66 @@
+from typing import Protocol
+from flask import json
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import BackendApplicationClient
+from dotenv import load_dotenv
+import os
+
+class CommsProtocol(Protocol):
+
+    def post_message(self) -> None:
+        pass
+
+
+class Cliq:
+
+    oauthlib_conn: OAuth2Session
+
+    def __init__(self) -> None:
+        """Initialise the Cliq instance, primarily to setup the
+        OAuth2.0 credentials and make sure they are authorised."""
+
+        load_dotenv()
+        client_id = os.getenv("Z_CLIENT_ID")
+        client_secret = os.getenv("Z_CLIENT_SECRET")
+        scope = os.getenv("Z_SCOPE")
+        refresh_token = os.getenv("Z_REFRESH_TOKEN")
+        
+        client = BackendApplicationClient(client_id=client_id,refresh_token=refresh_token)
+        self.oauthlib_conn = OAuth2Session(client=client)
+        if not self.oauthlib_conn.authorized:
+            self.oauthlib_conn.fetch_token(
+                token_url=f"https://accounts.zoho.{os.getenv('Z_REGION')}/oauth/v2/token",
+                client_id=client_id,
+                client_secret=client_secret,
+                scope=scope
+            )
+    
+    def post_message(self, channel_id: str, message: dict) -> None:
+        url = f"https://cliq.zoho.{os.getenv('Z_REGION')}/api/v2/channelsbyname/{channel_id}/message?bot_unique_name={os.getenv('Z_CLIQ_BOT_NAME')}"
+        data = json.dumps(message)
+        response = self.oauthlib_conn.post(url, data=data)
+        print(response.status_code, response.content)
+
+
+if __name__ == "__main__":
+    print("Testing Cliq authorisation and credentials")
+    cliq = Cliq()
+    print("Testing sending a message to the set Cliq channel")
+    message = {
+        "text": "I have gathered this file as asked, it is ready to view.",
+        "card": {
+            "theme": "modern-inline"
+        },
+        "buttons": [{
+            "label": "View file",
+            "type": "+",
+            "action": {
+                "type": "open.url",
+                "data": {
+                    "web": "https://www.fridgefreezerdirect.co.uk"
+                }
+            }
+        }]
+    }
+    cliq.post_message(os.getenv("Z_CLIQ_CHANNEL_NAME"), message)
+    
